5e766659cee4b8bbb72507670446082098d35ac5
Alexandre Duros <alexandre.duros@gmail.com>
2015-10-06 23:15:31 +0200

    initial commit for schedule listing

---
+++ index.js
@@ -0,0 +1,35 @@
+#!/usr/local/bin/node
+
+var program = require("commander");
+var FFBBLogin = require("./login");
+var FFBBSchedule = require("./schedule");
+
+function credentials(val) {
+  var creds = val.split(":");
+  return {
+    login: creds[0],
+    password: creds[1]
+  }
+}
+
+function dateRange(val) {
+  var range = val.split("...");
+  return {
+    start: creds[0],
+    end: creds[1]
+  }
+}
+
+
+program
+  .version("1.0.0")
+  .option("-l, --login <login>:<password>",    "set FFBB extranet credentials", credentials)
+  .option("-s, --schedule <start>...<end>", "get schedule for the given start and end date")
+  .parse(process.argv);
+
+if (program.schedule) {
+  FFBBLogin.authenticateFBI(program.login)
+    .then(FFBBSchedule.listGames)
+    .then(function(games) { console.log("next games", games); })
+}
+
---
+++ schedule.js
@@ -0,0 +1,43 @@
+var Promise = require("bluebird");
+var http = require("http");
+
+function listGames(options) {
+  var start = options.start,
+      end = options.end,
+      cookie = options.cookie;
+
+  return new Promise(function(resolve, reject) {
+    var options = {
+      hostname: 'extranet.ffbb.com',
+      port: 80,
+      path: '/fbi/rechercherRepartitionSaisieOfficiel.do?action=executeRecherche&rechercherRepartitionSaisieOfficielsBean.dateDebutPeriode=02%2F10%2F2015&rechercherRepartitionSaisieOfficielsBean.dateFinPeriode=30%2F06%2F2016&sEcho=1&iColumns=9&sColumns=&iDisplayStart=0&iDisplayLength=20&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&iSortingCols=0&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true&bSortable_4=true&bSortable_5=true&bSortable_6=true&bSortable_7=true&bSortable_8=true&_=1444138254003',
+      method: 'GET',
+      headers: {
+        'Cookie': cookie,
+      }
+    };
+
+    var req = http.request(options, function(res) {
+      var body = "";
+      res.setEncoding('utf8');
+
+      res.on('data', function (chunk) {
+        body += chunk;
+      });
+      res.on('end', function() {
+        resolve(body);
+      })
+
+    });
+
+    req.on('error', function(e) {
+      reject(e.message);
+    });
+
+    req.end();
+  })
+}
+
+module.exports = {
+  listGames
+};
