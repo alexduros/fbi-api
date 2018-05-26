98fe43a692c60c6d53c2230ce2d099a101cdcf03
Alexandre Duros <alexandre.duros@gmail.com>
2018-05-27 01:48:40 +0200

    :tada: added appointments listing

---
+++ test/appointments.js
@@ -0,0 +1,22 @@
+import assert from 'assert';
+import { authenticateFBISession } from '../src/auth.js';
+import { getAppointmentsByDate } from '../src/appointments.js';
+import { username, password } from './credentials';
+
+
+describe('Appointments', () => {
+  describe('List appointements by date', () => {
+    it('should retrieve appointments given valid dates', (done) => {
+      authenticateFBISession({ username, password })
+        .then(({ sessionId }) =>
+          getAppointmentsByDate(sessionId)({
+            startDate: '01/01/2018',
+            endDate: '30/06/2018',
+          }))
+        .then(({ count, games }) => {
+          assert(count > 0);
+          done();
+        });
+    }).timeout('2s');
+  })
+});
