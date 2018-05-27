c5e68270d5a6c286cc5f3d8c0219d22f35293191
Alexandre Duros <alexandre.duros@gmail.com>
2018-05-27 23:27:28 +0200

    :bug: startDate and endDate not in url query

--- src/appointments.js
+++ src/appointments.js
@@ -34,12 +34,12 @@ const extractGamesFromHTML = compose(
   JSON.parse
 );
 
-const getAppointmentsByDate = (sessionId) => () => {
+const getAppointmentsByDate = (sessionId) => ({ startDate, endDate }) => {
   return new Promise(function(resolve, reject) {
     var options = {
       hostname: 'extranet.ffbb.com',
       port: 443,
-      path: '/fbi/rechercherRepartitionSaisieOfficiel.do?action=executeRecherche&rechercherRepartitionSaisieOfficielsBean.dateDebutPeriode=" + encodeURIComponent(startDate) + "&rechercherRepartitionSaisieOfficielsBean.dateFinPeriode=" + encodeURIComponent(endDate) + "&sEcho=1&iColumns=9&sColumns=&iDisplayStart=0&iDisplayLength=20&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&iSortingCols=0&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true&bSortable_4=true&bSortable_5=true&bSortable_6=true&bSortable_7=true&bSortable_8=true',
+      path: `/fbi/rechercherRepartitionSaisieOfficiel.do?action=executeRecherche&rechercherRepartitionSaisieOfficielsBean.dateDebutPeriode=${encodeURIComponent(startDate)}&rechercherRepartitionSaisieOfficielsBean.dateFinPeriode=${encodeURIComponent(endDate)}"&sEcho=1&iColumns=9&sColumns=&iDisplayStart=0&iDisplayLength=20&mDataProp_0=0&mDataProp_1=1&mDataProp_2=2&mDataProp_3=3&mDataProp_4=4&mDataProp_5=5&mDataProp_6=6&mDataProp_7=7&mDataProp_8=8&iSortingCols=0&bSortable_0=true&bSortable_1=true&bSortable_2=true&bSortable_3=true&bSortable_4=true&bSortable_5=true&bSortable_6=true&bSortable_7=true&bSortable_8=true`,
       method: 'GET',
       headers: {
         'cookie': sessionId,
